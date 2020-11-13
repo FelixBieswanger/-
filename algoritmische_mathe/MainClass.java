fc375dc87d02acf1be1430d4f8c1dc001b8d8df6
FelixBieswanger <felix.bieswanger@googlemail.com>
2020-11-13 15:51:50 +0100

    -

--- algoritmische_mathe/MainClass.java
+++ algoritmische_mathe/MainClass.java
@@ -1,11 +1,8 @@
 package algoritmische_mathe;
 
 public class MainClass {
-    public static void main(String[] args){
-        System.out.println("Hello");
-
-
-
+    public static void main(String[] args) {
+        System.out.println("Hello World");
 
     }
 }
